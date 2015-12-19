9297f053d73a24a10647c6200d52a19b141d86ad
Gary Pang <gary.pang@gmail.com>
2015-12-19 03:43:16 -0500

    Init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsStatefulModalsWithUiRouterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularJsStatefulModalsWithUiRouterApp
+ */
+angular.module('angularJsStatefulModalsWithUiRouterApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsStatefulModalsWithUiRouterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularJsStatefulModalsWithUiRouterApp
+ */
+angular.module('angularJsStatefulModalsWithUiRouterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
