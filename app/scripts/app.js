b6f77215708745d8493fa6aec92d367f3b75dcb4
Gary Pang <gary.pang@gmail.com>
2015-12-21 03:38:51 -0500

    Update app.js
    Users can close modal by clicking outside or pressing ESC. Prevents
    modal from closing by accident if user clicks inside.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,15 +19,35 @@ angular
     'ui.router'
   ])
   .config(function ($stateProvider) {
+      $stateProvider.state("Default", {});
+
       $stateProvider.state("Modal", {
           views: {
               "modal": {
                   templateUrl: "modal.html"
               }
           },
+          // Closes modal if user clicks outside or presses ESC
+          onEnter: ["$state", function($state) {
+              $(document).on("keyup", function(e) {
+                  if(e.keyCode == 27) {
+                      $(document).off("keyup");
+                      $state.go("Default");
+                  }
+              });
+
+              $(document).on("click", ".Modal-backdrop, .Modal-holder", function(){
+                  $state.go("Default");
+              });
+
+              // Prevents modal from closing if user clicks inside
+              $(document).on("click", ".Modal-box, .Modal-box *", function(e){
+                  e.stopPropagation();
+              });
+          }],
           abstract: true
       });
-      
+
       $stateProvider.state("Modal.confirmAddToCart", {
           views: {
               "modal": {
@@ -35,4 +55,4 @@ angular
               }
           }
       });
-});
+  });
